2c74fa179cfe85be6fa3fe3ed228152fb51b49d3
Jacky <jackymayonaise@gmail.com>
2016-01-11 18:35:22 -0500

    Added digits and basic operators

--- swiftCalculator/ViewController.swift
+++ swiftCalculator/ViewController.swift
@@ -3,6 +3,7 @@
 //  swiftCalculator
 //
 //  Created by Jacky Moon on 2016-01-11.
+//  All credits goes to Paul Hegarty's demo used as a reference
 //  Copyright © 2016 Jacky Moon. All rights reserved.
 //
 
@@ -10,15 +11,75 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var operandStack = Array<Double>()
+    var userIsPressingButton = false
+   
+    
     @IBOutlet weak var calculatorDisplay: UILabel!
 
+  
     @IBAction func calculatorDigit(sender: UIButton) {
-        let digitValue = sender.currentTitle!
-        print("\(digitValue)")
-        calculatorDisplay.text = calculatorDisplay.text! + digitValue
-        
+        let digit = sender.currentTitle!
+        //used for inital debugging print("\(digit)")
+        if userIsPressingButton {
+            calculatorDisplay.text = calculatorDisplay.text! + digit
+        }
+        else{
+            calculatorDisplay.text = digit
+            userIsPressingButton = true
+        }
+    
     }
     
+    @IBAction func operatorType(sender: UIButton) {
+        let operatorValue = sender.currentTitle!
+        switch operatorValue {
+            //last operation could go outside
+        case "×": performOperation(){$0 * $1}
+        case "÷": performOperation(){$1 / $0}
+        case "+": performOperation(){$0 + $1}
+        case "−": performOperation(){$1 - $0}
+        case "√": performOperation(){$0}
+            default: break
+        }
+    }
+    
+    @nonobjc func performOperation(operation: (Double,Double) -> Double ) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enterButton()
+        }
+    }
+    
+    @nonobjc func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enterButton()
+        }
+    }
+    
+    
+    @IBAction func clearButton(sender: UIButton) {
+        operandStack.All()
+        userIsPressingButton = false
+    }
+    var displayValue : Double{
+        get {
+            return NSNumberFormatter().numberFromString(calculatorDisplay.text!)!.doubleValue
+        }
+        set {
+            calculatorDisplay.text = "\(newValue)"
+            userIsPressingButton = false
+        }
+    }
+    @IBAction func enterButton() {
+        //resets the display
+        userIsPressingButton = false
+        operandStack.append(displayValue)
+        print("operandStack = \(operandStack)")
+        
+        
+    }
     
     
     
